12be16b1f780411f5552a909bd0ec8fb2a5bbf6c
Olof Tingskull <olof.tingskull@gmail.com>
2025-12-23 18:25:25 +0100

    added type before prisma

--- app/api/journal/submit/route.ts
+++ app/api/journal/submit/route.ts
@@ -1,4 +1,4 @@
-import { Prisma } from '@prisma/client'
+import { type Prisma } from '@prisma/client'
 import { type NextRequest, NextResponse } from 'next/server'
 import { withAuth } from '@/lib/middleware'
 import { prisma } from '@/lib/prisma'
