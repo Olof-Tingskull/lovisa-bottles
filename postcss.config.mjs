4410878f6dce85a1698dc42260fa17adfc851e9d
Olof Tingskull <olof.tingskull@gmail.com>
2025-12-24 01:31:08 +0100

    mobile and css fix

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,13 +1,6 @@
-import { fileURLToPath } from 'url'
-import path from 'path'
-
-const __dirname = path.dirname(fileURLToPath(import.meta.url))
-
 const config = {
   plugins: {
-    '@tailwindcss/postcss': {
-      base: __dirname,
-    },
+    '@tailwindcss/postcss': {},
   },
 }
 
